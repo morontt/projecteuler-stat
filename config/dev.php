157accf7b90e3443f2d872b4d94f13c869db7dcc
Alexander Harchenko <morontt@gmail.com>
2016-05-31 23:00:16 +0300

    reformat code

--- config/dev.php
+++ config/dev.php
@@ -3,11 +3,11 @@
 use Silex\Provider\WebProfilerServiceProvider;
 
 // include the prod configuration
-require __DIR__.'/prod.php';
+require __DIR__ . '/prod.php';
 
 // enable the debug mode
 $app['debug'] = true;
 
-$app->register(new WebProfilerServiceProvider(), array(
-    'profiler.cache_dir' => __DIR__.'/../var/cache/profiler',
-));
+$app->register(new WebProfilerServiceProvider(), [
+    'profiler.cache_dir' => __DIR__ . '/../var/cache/profiler',
+]);
